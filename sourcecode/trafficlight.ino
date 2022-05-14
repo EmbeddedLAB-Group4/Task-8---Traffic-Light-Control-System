1e55f0979ab31a27239486268c5d215cc311cb6f
EmbeddedLAB-Group4 <104303685+EmbeddedLAB-Group4@users.noreply.github.com>
2022-05-14 19:40:33 +0200

    Update trafficlight.ino

--- sourcecode/trafficlight.ino
+++ sourcecode/trafficlight.ino
@@ -37,6 +37,7 @@ void setup() {
     digitalWrite(petred1, HIGH);
     digitalWrite(petgreen1, LOW);
     attachInterrupt(digitalPinToInterrupt(3),stop1,RISING);
+  // interupt checking in pin 3 for rising edge
   
     pinMode(red2, OUTPUT);
     pinMode(yellow2, OUTPUT);
@@ -52,6 +53,7 @@ void setup() {
     digitalWrite(petgreen2, LOW);
     
     attachInterrupt(digitalPinToInterrupt(2),stop2,RISING);
+  // interupt checking in pin 3 for rising edge
     Serial.begin(9600);
     car1 =0;
     car2 =0;
@@ -66,6 +68,7 @@ void setup() {
 void loop() {
   
  if(s1 == 1 && s2== 0 ){
+   // checking if button 1 is pressed 
         digitalWrite(buzzer1, HIGH);
         digitalWrite(petred1, LOW);
         digitalWrite(petgreen1, HIGH);
@@ -78,12 +81,13 @@ void loop() {
         digitalWrite(petgreen1, LOW);
         s1 = 0;
         changeLights1(1);
-        queue1push(3);
-            queue2pop(3);
+        queue1push(3);  // push 3 cars in queue 1
+        queue2pop(3);  // pop 3 cars in queue 2
         
         
         }else if(s2 ==1 && s1==0)
     {
+   // checking if button 2 is pressed 
         digitalWrite(buzzer2, HIGH);
         digitalWrite(petred2, LOW);
         digitalWrite(petgreen2, HIGH);
@@ -96,12 +100,13 @@ void loop() {
         digitalWrite(petgreen2, HIGH);
         s2 = 0;
         changeLights1(1);
-        queue1pop(3);
-        queue2push(3);  
+        queue1pop(3);   // pop 3 cars in queue 1
+        queue2push(3);  // push 3 cars in queue 2
     
         
     }else if(s1 ==1 && s2==1)
             {
+   // checking if button 1 and button2  is pressed 
         digitalWrite(buzzer1, HIGH);
         digitalWrite(buzzer2, HIGH);
         digitalWrite(petred1, LOW);
@@ -118,8 +123,8 @@ void loop() {
         digitalWrite(petgreen1, LOW);
         digitalWrite(petred2, HIGH);
         digitalWrite(petgreen2, LOW);
-        queue1push(3);
-        queue2push(3);
+        queue1push(3);// push 3 cars in queue 1
+        queue2push(3);// pop 3 cars in queue 2
         s1 =0;
         s2 = 0;
         }
@@ -127,18 +132,19 @@ void loop() {
   
     else {
           changeLights1(0);
-            queue1push(5);
-            queue2pop(5);
+            queue1push(5); // push 5 cars in queue 1
+            queue2pop(5);    // pop 5 cars in queue 2
             changeLights1(1);
-        queue1pop(5);
-        queue2push(5);      
+        queue1pop(5);  // pop 5 cars in queue 1
+        queue2push(5); // push 5 cars in queue 2    
          }
   
    
  }
 
 void changeLights1(int a){
-    // green off, yellow on for 2 seconds
+    // green off, yellow on for 2 seconds and then to red for 9sec
+  
 
   
   if(a == 0)
@@ -149,8 +155,8 @@ void changeLights1(int a){
     digitalWrite(green2, LOW);
     digitalWrite(yellow2, HIGH);
     digitalWrite(red2, LOW);
-    // this is to check if button is still pressed while delaying 
     for(long int i = 0; i < 2000; i++){
+// delay 2 sec
       delay(1);
       }
     
@@ -163,6 +169,7 @@ void changeLights1(int a){
     digitalWrite(red2, LOW);
     
     for(long int i = 0; i < 9000; i++){
+      
       delay(1);
       }
     
@@ -225,11 +232,13 @@ void changeLights1(int a){
 void stop1()
 {
   s1 = 1;
+  // change the variable to 1 to save the interrupt
 }
 
 void stop2()
 {
   s2 = 1;
+  // change the variable to 1 to save the interrupt
 }
 
 void queue1push(int a)
